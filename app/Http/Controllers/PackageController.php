5c2ae179f68cdbf25d1f0c268a3b9c96fb036d5e
pieralexandre01 <pier-alexandre.matteau@hotmail.com>
2023-03-15 09:53:24 -0400

    Changement de nom du controller et du model Reservation pour Package

--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ReservationController extends Controller
+class PackageController extends Controller
 {
     // Afficher la page des forfaits
 }
