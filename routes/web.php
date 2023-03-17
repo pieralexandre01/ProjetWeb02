24b652ac3d90a2e5507fac453f4e3e4fbe4636c7
patricia-m <patricia.massie@outlook.com>
2023-03-17 14:50:30 -0400

    Création des routes admin

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PackageController;
+use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\SiteController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\AuthenticateAdmin;
@@ -57,10 +58,10 @@ Route::middleware([RedirectUserIfAuthenticated::class])->group(function () {
 
     Route::get('/account/create', [AuthController::class, 'createAccount'])
         ->name('account-create');
-
     });
 
 // Enregistrement d'un user
+// ******************************************************************************************************************************************** todo!
 Route::post('/account/create',[AuthController::class, 'storeAccount'])
     ->name('account-create');
 
@@ -69,15 +70,21 @@ Route::get('/dashboard', [UserController::class, 'showDashboard'])
     ->name('dashboard')
     ->middleware('auth');
 
-// ------------------------------------------------------------------------------------ Déconnexion
-
+// ------------------------------------------------------------------------------------ Public et admin
+// Déconnexion
 Route::post('/logout',[AuthController::class, 'logout'])
     ->name('logout');
+
+// Supprimer une réservation (public et admin)
+Route::post('/reservations/delete{id}', [ReservationController::class, 'destroy'])
+    ->name('reservation-delete')
+    ->middleware('auth');
 
 // ------------------------------------------------------------------------------------ Section admin
 
 Route::middleware([AuthenticateAdmin::class])->group(function () {
 
+    // Connexion ------------------------------------------------------------
     Route::get('/admin/login', [AuthController::class, 'showLoginAdmin'])
         ->name('admin-login')
         // Ajout d'un middleware qui redirige au dashboard si déjà connecté
@@ -88,38 +95,69 @@ Route::middleware([AuthenticateAdmin::class])->group(function () {
     Route::get('/admin', [AdminController::class, 'showDashboardAdmin'])
         ->name('admin-dashboard');
 
-    // Activities
 
-    // Articles
+    // ******************************************************************************************************************************************** todo!
 
-    // Users
-    // créer - admin
-    Route::get('/admin/create{id}', [AuthController::class, 'createAdmin'])
+    // Users -----------------------------------------------------------------
+    // affichge formulaire création admin
+    Route::get('/admin/create', [AuthController::class, 'createAdmin'])
         ->name('admin-create');
 
-    // modif - admin et public
-    Route::get('/admin/edit', [AuthController::class, 'editAdmin'])
-        ->name('admin-edit');
+    // modif
+    // affichage formulaire modif user admin
+    Route::get('/admin/edit{id}', [AdminController::class, 'editAdmin'])
+    ->name('admin-edit');
 
-    Route::get('/user/edit', [AuthController::class, 'editUser'])
-        ->name('user-edit');
+    // affichage formulaire modif user public
+    Route::get('/user/edit{id}', [AdminController::class, 'editUser'])
+    ->name('user-edit');
+
+    // envoie du formulaire de modif d'un user
+    Route::post('/user/update{id}', [AdminController::class, 'updateUser'])
+        ->name('user-update');
+
     // supprimer - admin et public
+    Route::post('/user/delete{id}', [AdminController::class, 'destroy'])
+        ->name('user-delete');
 
 
+    // Articles --------------------------------------------------------------
+    // création
+    Route::get('/articles/create', [ArticleController::class, 'create'])
+        ->name('article-create');
+
+    Route::post('/articles/create', [ArticleController::class, 'store'])
+        ->name('article-store');
+
+    // modif
+    Route::get('/articles/edit{id}', [ArticleController::class, 'edit'])
+        ->name('article-edit');
+
+    Route::post('/articles/edit{id}', [ArticleController::class, 'update'])
+        ->name('article-update');
+
+    // supprimer
+    Route::post('/articles/delete{id}', [AdminController::class, 'destroy'])
+        ->name('article-delete');
 
 
-    // route : /whatever/create
-    // create - get
-    // store - post
+    // Activities ------------------------------------------------------------
+    // création
+    Route::get('/activities/create', [ActivityController::class, 'create'])
+     ->name('activity-create');
 
-    // route : /whatever/edit/{id}
-    // edit - get
-    // update - post
+    Route::post('/activities/create', [ActivityController::class, 'store'])
+        ->name('activity-store');
 
-    // destroy - get
-    // Softdelete = block
+    // modif
+    Route::get('/activities/edit{id}', [ActivityController::class, 'edit'])
+        ->name('activity-edit');
+
+    Route::post('/activities/edit{id}', [ActivityController::class, 'update'])
+        ->name('activity-update');
+
+    // supprimer
+    Route::post('/activities/delete{id}', [ActivityController::class, 'destroy'])
+        ->name('activity-delete');
 
 });
-
-
-
