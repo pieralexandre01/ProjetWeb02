0fb2c7b5df2ef73f5ff56ac41f75d439a846c918
jacleb <jacqueline.lebel21@gmail.com>
2023-03-18 16:01:35 -0400

    css + html pour la page de login public + restructuration / organisation des dossiers et fichiers css

--- resources/views/auth/public/login.blade.php
+++ resources/views/auth/public/login.blade.php
@@ -5,12 +5,27 @@
 
     <main>
 
-        <form action="{{ route('login') }}" method="post">
-            @csrf
-            <input name="email" type="text">
-            <input name="password" type="password">
-            <input type="submit">
-        </form>
+        <div class="container mt-5">
+            <div class="form-container d-inline-block">
+
+                <h3 class="text-center mb-4">LOGIN</h3>
+
+                <form action="{{ route('login') }}" method="post" class="d-flex flex-column align-items-start">
+                    @csrf
+                    <div class="d-flex flex-column mb-5 w-100">
+                        <label for="email" class="mb-1">E-MAIL</label>
+                        <input id="email" name="email" type="text">
+                    </div>
+                    <div class="d-flex flex-column mb-5 w-100">
+                        <label for="password" class="mb-1">PASSWORD</label>
+                        <input id="password" name="password" type="password">
+                    </div>
+                    <input type="submit" class="align-self-center">
+                </form>
+
+                <p class="mt-4">don't have an account? <a href="{{ route('account-create') }}" class="text-decoration-underline">create one</a></p>
+            </div>
+        </div>
 
     </main>
 
--- resources/views/components/public/layout.blade.php
+++ resources/views/components/public/layout.blade.php
@@ -9,7 +9,8 @@
     <link rel="stylesheet" href="https://use.typekit.net/lpp7zdj.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/public.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/public/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/public/login.css') }}">
 </head>
 
 <body>
