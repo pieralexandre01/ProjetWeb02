f92f9c82a4582ad6b9bb20a2aefa442c27510cff
pieralexandre01 <pier-alexandre.matteau@hotmail.com>
2023-03-18 14:53:01 -0400

    Correction des problèmes (en théorie)

--- resources/views/admin/form/create/admin.blade.php
+++ resources/views/admin/form/create/admin.blade.php
@@ -1,30 +1,26 @@
 <x-admin.layout>
-    {{-- <x-slot name="titre"></x-slot> --}}
-    <form action="{{ route('admin-update', ['id' => $user->id]) }}" method="post">
-        @csrf
-        <input type="hidden" name="privilege_type" value="public">
-        <input type="text" name="first_name" value="{{ $user->first_name }}">
-        <input type="text" name="last_name" value="{{ $user->last_name }}">
-        <input type="email" name="email" value="{{ $user->email }}">
-        <input type="password" name="password" >
-        <input type="password" name="password_confirm">
-        <input type="submit">
-    </form>
 
-<<<<<<< HEAD
-    @error('email')
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $message }}</strong>
-        </span>
-    @enderror
-
-</x-admin.layout>
-=======
     <x-admin.header />
 
-    <main></main>
+    <main>
+        <form action="{{ route('admin-update', ['id' => $user->id]) }}" method="post">
+            @csrf
+            <input type="hidden" name="privilege_type" value="public">
+            <input type="text" name="first_name" value="{{ $user->first_name }}">
+            <input type="text" name="last_name" value="{{ $user->last_name }}">
+            <input type="email" name="email" value="{{ $user->email }}">
+            <input type="password" name="password" >
+            <input type="password" name="password_confirm">
+            <input type="submit">
+        </form>
+
+        @error('email')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+    </main>
 
     <x-admin.footer />
 
-</x-layout>
->>>>>>> JacDev
+</x-admin.layout>
--- resources/views/admin/form/modify/user.blade.php
+++ resources/views/admin/form/modify/user.blade.php
@@ -1,18 +1,4 @@
 <x-admin.layout>
-    {{-- <x-slot name="titre"></x-slot> --}}
-<<<<<<< HEAD
-=======
-    <form action="{{ route('user-update', ['id' => $user->id]) }}" method="post">
-        @csrf
-        <input type="hidden" name="privilege_type" value="public">
-        <input type="text" name="first_name" value="{{ $user->first_name }}">
-        <input type="text" name="last_name" value="{{ $user->last_name }}">
-        <input type="email" name="email" value="{{ $user->email }}">
-        <input type="password" name="password" >
-        <input type="password" name="password_confirm">
-        <input type="submit">
-    </form>
->>>>>>> PaDev
 
     <x-admin.header />
 
