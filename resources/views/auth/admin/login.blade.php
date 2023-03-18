e839cc4b518e9a86ef99315eb27d9ab0fa6ad403
jacleb <jacqueline.lebel21@gmail.com>
2023-03-18 14:32:53 -0400

    Ajout d'un détail responsive en super écran :) pour le dropdown de l'icône user dans le header public. Compléter la préparation des views avec les bonnes balises

--- resources/views/auth/admin/login.blade.php
+++ resources/views/auth/admin/login.blade.php
@@ -1,6 +1,19 @@
-<form action="{{ route('login') }}" method="post">
-    @csrf
-    <input name="email" type="text">
-    <input name="password" type="password">
-    <input type="submit">
-</form>
+<x-admin.layout>
+    {{-- <x-slot name="titre"></x-slot> --}}
+
+    <x-admin.header />
+
+    <main>
+
+        <form action="{{ route('login') }}" method="post">
+            @csrf
+            <input name="email" type="text">
+            <input name="password" type="password">
+            <input type="submit">
+        </form>
+
+    </main>
+
+    <x-admin.footer />
+
+</x-admin.layout>
