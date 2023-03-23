3f6e522ac14240bbeb3d99854dde9ad0e5edf8ca
patricia-m <patricia.massie@outlook.com>
2023-03-23 14:38:39 -0400

    début de l'ajout de la méthode pour modifier un password (à compléter - comportement (redirection) à déterminer)

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -87,11 +87,11 @@ class AdminController extends Controller
         $user->email = $request->email;
 
         // Gère le type de user à modifier
-        if($request->privilege_type == 'admin') {
-            $user->privilege_id = 1;
-        } elseif ($request->privilege_type == 'public') {
-            $user->privilege_id = 2;
-        }
+        // if($request->privilege_type == 'admin') {
+        //     $user->privilege_id = 1;
+        // } elseif ($request->privilege_type == 'public') {
+        //     $user->privilege_id = 2;
+        // }
 
         // Enregistrement
         $user->save();
@@ -100,6 +100,49 @@ class AdminController extends Controller
             ->route('admin-dashboard')
             ->with('account-edit', "The user's account has been succesfully modified");
     }
+
+
+    public function editPassword($id) {
+        return view('admin.form.modify.password', [
+            "title" => "MW | Admin | Modify PW",
+            "page" => "edit-password",
+            "user" => User::findOrFail($id),
+        ]);
+    }
+
+
+    public function updatePassword(Request $request, $id) {
+        // Valider
+        $request->validate([
+            'password' => 'required',
+            'password_confirm' => 'required|same:password'
+        ], [
+            'password.required' => 'The password is required',
+            'password_confirm.required' => 'The password confirmation is required',
+            'password_confirm.same' => 'The passwords do not match'
+        ]);
+
+        $user = User::findOrFail($id);
+
+        $user->password = $request->password;
+        $user->password_confirm = $request->password_confirm;
+
+        // Enregistrement
+        $user->save();
+
+        // Retourner à la page de modif du user admin-edit OU user-edit ?
+        // /admin/edit/{id}
+        // /admin/user/edit/{id}
+
+        $previous_url = $request->header('referer');
+        // problème : le previous_url sera le form de changement de pw qui est le même pour les 2 types de user.
+        // utiliser le privilege->id pour déterminer sur quelle page retouner??
+
+        // return redirect()
+        //     ->back
+        //     ->with('password-edit', "The user's password has been succesfully modified");
+    }
+
 
     /**
      * Soft delete (bloque) un utilisateur
