ad4d2b50e8be083c8655444a251bda925b76ed87
patricia-m <patricia.massie@outlook.com>
2023-03-24 14:13:18 -0400

    correction du lien css dans le dashboard admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-admin.layout>
     <x-slot name="title">{{ $title }}</x-slot>
-    <x-slot name="css_file">admin/admin_dashboard</x-slot>
+    <x-slot name="css_file">admin_dashboard</x-slot>
 
     <x-admin.header />
 
