21bddfb7969ccb18634ed17390e472085a596915
pieralexandre01 <pier-alexandre.matteau@hotmail.com>
2023-03-24 13:45:40 -0400

    correction dans les json, car ils faisaient crasher la bdd + page create account admin non-terminée

--- resources/views/admin/form/create/admin.blade.php
+++ resources/views/admin/form/create/admin.blade.php
@@ -5,22 +5,67 @@
     <x-admin.header />
 
     <main>
-        <form action="{{ route('account-create') }}" method="post">
-            @csrf
-            {{-- <input type="hidden" name="privilege_type" value="admin"> --}}
-            <input type="text" name="first_name">
-            <input type="text" name="last_name">
-            <input type="email" name="email">
-            <input type="password" name="password">
-            <input type="password" name="password_confirm">
-            <input type="submit">
-        </form>
 
-        @error('email')
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-            </span>
-        @enderror
+        <div class="page_title">
+            <h1 class="text-end">ADD: ADMIN</h1>
+
+            <div class="title_decoration">
+                <div class="circle"></div>
+            </div>
+        </div>
+
+        <div class="container ">
+            <div id="account_create" class="form-container border_box py-2">
+
+                <h3 class="text-center mt-sm-3">CREATE AN ACCOUNT</h3>
+
+                <form action="{{ route('account-create') }}" method="post"
+                    class="d-flex flex-column align-items-start gap-2 gap-md-0 w-100 mt-3 mt-sm-4">
+                    @csrf
+
+                    <div class="d-flex flex-column flex-md-row align-items-start gap-2 gap-md-5 mb-md-3 mb-lg-4 w-100">
+                        <div class="d-flex flex-column w-100">
+                            <label for="first_name" class="mb-1">FIRST NAME</label>
+                            <input id="first_name" name="first_name" type="text">
+                            <x-msg-error field="first_name" />
+                        </div>
+                        <div class="d-flex flex-column w-100">
+                            <label for="last_name" class="mb-1">LAST NAME</label>
+                            <input id="last_name" name="last_name" type="text">
+                            <x-msg-error field="last_name" />
+                        </div>
+                    </div>
+
+                    <div class="d-flex flex-column mb-md-3 mb-lg-4 w-100">
+                        <label for="email" class="mb-1">E-MAIL</label>
+                        <input id="email" name="email" type="text">
+                        <x-msg-error field="email" />
+                    </div>
+
+                    <div class="d-flex flex-column flex-md-row align-items-start gap-2 gap-md-5 mb-md-4 w-100">
+                        <div class="d-flex flex-column w-100">
+                            <label for="password" class="mb-1">PASSWORD</label>
+                            <input id="password" name="password" type="password">
+                            <x-msg-error field="password" />
+                        </div>
+                        <div class="d-flex flex-column w-100">
+                            <label for="password_confirm" class="mb-1">CONFIRM PASSWORD</label>
+                            <input id="password_confirm" name="password_confirm" type="password">
+                            <x-msg-error field="password_confirm" />
+                        </div>
+                    </div>
+
+                    <input type="submit" class="align-self-center mt-3 mt-md-0">
+                </form>
+
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+        </div>
+
     </main>
 
     <x-footer />
