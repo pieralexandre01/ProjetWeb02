2217c0f05b00ea8abe482b0a6891a5e88b8b75bb
patricia-m <patricia.massie@outlook.com>
2023-03-23 20:51:02 -0400

    début de squelette HTML & CSS pour les pages packages & dashboard admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,7 +4,79 @@
 
     <x-admin.header />
 
-    <main></main>
+    {{-- comment arranger le footer qui embarque sur le contenu? --}}
+
+    {{-- à mettre dans le fichier css ------------------------------------------------------------------------------ --}}
+    <style>
+        .accordion-button,
+        .accordion-button:not(.collapsed) {
+            background-color: transparent;
+        }
+
+        .accordion-button:not(.collapsed)::after,
+        .accordion-button.collapsed::before {
+            color: #fff;
+        }
+
+        div.arrow {
+            background-color: #fff;
+            width: 30px;
+            /* changer pour la bonne couleur */
+            color: blue;
+        }
+    </style>
+    {{-- --------------------------------------------------------------------------------------------------------- --}}
+
+    <main>
+
+        <div class="container pt-5 mt-5">
+
+            <section id="users_admin">
+                <div class="accordion">
+
+                    <div class="collapsed ps-0 d-flex justify-content-between" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        {{-- mettre le style du h3 au h2 --}}
+                        <h2 class="text-white">admin</h2>
+                        <div class="arrow d-flex justify-content-center align-items-center">
+                            <div>V</div> {{-- toggle sur une flèche qui change de direction (et couleur?) --}}
+                        </div>
+                    </div>
+                    <hr class="mt-0">
+
+                    <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne"
+                        data-bs-parent="#accordionExample">
+                        {{-- styliser le bouton --}}
+                        <div class="my-5"><a href="">add</a></div>
+
+                        @foreach ($users_admin as $user_admin)
+                            <div class="d-flex justify-content-between pt-3">
+
+                                <div class="infos d-flex">
+                                    {{-- ajuster les distances --}}
+                                    <p class="">{{ $user_admin->email }}</p>
+                                    <p class="">{{ $user_admin->first_name }}</p>
+                                    <p class="">{{ $user_admin->last_name }}</p>
+                                </div>
+
+                                <div class="buttons d-flex">
+                                    {{-- changer les couleurs et hover --}}
+                                    <a href="" class="me-4">modifier</a>
+                                    <a href="">supprimer</a>
+                                </div>
+
+                            </div>
+                            <hr>
+                        @endforeach
+                    </div>
+                </div>
+            </section>
+
+        </div>
+
+        </div>
+
+    </main>
 
     <x-footer />
 
--- resources/views/packages.blade.php
+++ resources/views/packages.blade.php
@@ -5,22 +5,56 @@
     <x-public.header :page="$page" />
 
     <main>
+        {{-- comment arranger le footer qui embarque sur le contenu? --}}
 
-        @foreach ($packages as $package)
-            <h2>{{ $package->title }}</h2>
-            <form action="{{ route('package-addtocart', $package->id) }}" method="post">
-                @csrf
-                <input type="hidden" name="package_id" value="{{ $package->id }}">
-                <select name="package_quantity">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                </select>
-                <input type="submit" value="add to cart">
-            </form>
-        @endforeach
+        <div class="page_title pb-5">
+            <h1 class="text-end">PACKAGES</h1>
 
+            <div class="title_decoration">
+                <div class="circle"></div>
+            </div>
+        </div>
+
+        <div class="container">
+
+            {{-- style à appliquer sur le premier forfait seulement --}}
+            {{-- border_box = est-ce une classe créé par Jackie puisqu'elle s'adapte bien au rose? --}}
+            <div class="border_box p-2">
+            </div>
+
+            @foreach ($packages as $package)
+                <div class="row">
+                    {{-- revoir les distances/largeurs --}}
+                    <div class="col-7">
+                        <h2>{{ $package->title }}</h2>
+                        {{-- ajuster la ligne --}}
+                        {{-- <div class="title_decoration">
+                            <div class="circle"></div>
+                        </div> --}}
+                        <p>{{ $package->description }}</p>
+
+                        {{-- AJOUTER LES AUTRES INFOS --}}
+
+                        {{-- styliser les boutons --}}
+                        <form action="{{ route('package-addtocart', $package->id) }}" method="post">
+                            @csrf
+                            <input type="hidden" name="package_id" value="{{ $package->id }}">
+                            <select name="package_quantity">
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                            </select>
+                            <input class="" type="submit" value="reserve">
+                            {{-- class de Jackie : read_more_button --}}
+                        </form>
+                    </div>
+
+                    <div class="col-7">
+                        <img src="" alt="">
+                    </div>
+                </div>
+            @endforeach
     </main>
 
     <x-public.footer />
