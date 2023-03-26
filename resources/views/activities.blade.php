793744bf5c350ed989ebe160239c440333594988
jacleb <jacqueline.lebel21@gmail.com>
2023-03-26 19:05:30 -0400

    travail sur la page d'activités en mode mobile (accordéon) HTML + CSS + appels dynamiques à la bdd

--- resources/views/activities.blade.php
+++ resources/views/activities.blade.php
@@ -13,58 +13,44 @@
             </div>
         </div>
 
-        <div class="container mt-5 px-4 px-md-0 d-lg-none">
-            <div class="accordion accordion-flush" id="accordionFlushExample">
+        <div class="activities container mt-5 px-4 px-md-0 d-lg-none">
+
+            <div class="accordion" id="activity_accordion">
+
+                @foreach ($activities as $activity)
 
                 <div class="accordion-item">
-                    <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                        meet & greet - VR
-                        </button>
-                    </h2>
-                    <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                        <div class="accordion-body">
-                            text
+                        <div class="accordion-header">
+
+                            {{-- dynamic aria-expanded on click -> true --}}
+                            <button class="accordion-button px-4" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{ $activity->id }}" aria-expanded="false" aria-controls="collapse{{ $activity->id }}">
+
+                                {{ $activity->title }}
+                            </button>
                         </div>
-                    </div>
-                </div>
+                        <div id="collapse{{ $activity->id }}" class="accordion-collapse collapse" data-bs-parent="#activity_accordion">
+                            <div class="accordion-body">
 
-                <div class="accordion-item">
-                    <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                        meet & greet - AI
-                        </button>
-                    </h2>
-                    <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                        <div class="accordion-body">
-                            text
+                                @if ($activity->subcategory != null)
+                                    {{-- en lettres majuscules: --}}
+                                    <h2>{{ $activity->subcategory }}</h2>
+                                    <div class="subcategory_bar mb-4"></div>
+                                @endif
+
+                                <div class="d-flex flex-column align-items-center">
+                                    <img src="{{ asset('/../' . $activity->image ) }}" alt="">
+                                    <div class="activity_date align-self-end mt-3 mb-3">{{ $activity->date }}</div>
+                                </div>
+                                <p class="mb-0">{{ $activity->description }}</p>
+                                {{-- ENLEVER ??? : <p>check your e-mail to access this event!</p> --}}
+                            </div>
                         </div>
-                    </div>
-                </div>
 
-                <div class="accordion-item">
-                    <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                        meet & greet - RBT
-                        </button>
-                    </h2>
-                    <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                        <div class="accordion-body">text</div>
                     </div>
-                </div>
-
-                <div class="accordion-item">
-                    <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
-                        tech talks
-                        </button>
-                    </h2>
-                    <div id="flush-collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                        <div class="accordion-body">text</div>
-                    </div>
-                </div>
+                    @endforeach
 
             </div>
+
         </div>
     </main>
 
