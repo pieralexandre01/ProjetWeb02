dcc2fd8330d092fc7f9feb3db1a2da87f697582e
jacleb <jacqueline.lebel21@gmail.com>
2023-03-27 13:48:44 -0400

    Ajustement à l'accordéon pour la page d'activités en mode mobile et intégration de la SPA pour la version desktop. Ajout du div id="app" au layout.blade.php

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 
 import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 
+// Header - navigation
 const is_open = ref(false)
 
 function toggleMenu() {
@@ -12,11 +13,19 @@ function toggleMenu() {
     }
 }
 
+// Activity page
+const activity = ref(0)
+
+// Homepage
+
+
+
 const root = {
     setup() {
         return {
             //Propriétés
             is_open,
+            activity,
 
             //Méthodes
             toggleMenu,
