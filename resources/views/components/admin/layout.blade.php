73a7e83a14ef4e5a7d217693975c7348d3b0c314
jacleb <jacqueline.lebel21@gmail.com>
2023-04-05 23:47:44 -0400

    Dernier push je l'ai fait sur le main par accident, oops!

--- resources/views/components/admin/layout.blade.php
+++ resources/views/components/admin/layout.blade.php
@@ -18,10 +18,9 @@
     {{ $slot }}
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     </script>
     <script src="{{ asset('js/main.js') }}" type="module"></script>
 
 </body>
-
 </html>
